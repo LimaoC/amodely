03874ecd58991a693b94aa8abcbea853784d02d0
LimaoC <80520563+LimaoC@users.noreply.github.com>
2022-01-28 15:47:28 +1000

    Add more constants for use in Plot.ly dashboard

--- src/lib/lib.py
+++ src/lib/lib.py
@@ -34,6 +34,8 @@ STRUCTURE = {
 
 # ----------------------- PLOTLY DASHBOARD CONSTANTS ------------------------ #
 
+TEMPLATE = "flatly"
+COLOR_PALETTE = px.colors.qualitative.Pastel
 DEFAULT_MEASURE = "CONVERSION_RATE"  # default on first load for graphs
 DEFAULT_DIMENSION = "STATE_CODE"  # default on first load for graphs
 DEFAULT_HOVER_DATA = {  # default on first load for graphs
@@ -45,8 +47,43 @@ DEFAULT_HOVER_DATA = {  # default on first load for graphs
         }
     ]
 }
-TEMPLATE = "flatly"
-COLOR_PALETTE = px.colors.qualitative.Pastel
+DEFAULT_FIGURE_LAYOUT = {
+    "title": {
+        "font_size": 20,
+    },
+    "margin": {
+        "b": 40,  # bottom margin
+        "t": 40,  # top margin
+    },
+}
+# HOVER_DATA:
+#   "average": add the average of the hovered data point on the anomaly subplot
+#   "point": add the hovered data point on the anomaly subplot
+# TICK_FORMAT:
+#   ",.0f" separate thousands with a comma instead of a "k"
+#   ".1%"  show percentages instead of decimals
+GRAPH_CONFIG = {
+    "QUOTE_VOLUME": {
+        "HOVER_DATA": "average",
+        "TICK_FORMAT": ",.0f"
+    },
+    "SALES_VOLUME": {
+        "HOVER_DATA": "average",
+        "TICK_FORMAT": ",.0f"
+    },
+    "QUOTE_PROPORTION": {
+        "HOVER_DATA": "point",
+        "TICK_FORMAT": ".2%"
+    },
+    "SALES_PROPORTION": {
+        "HOVER_DATA": "point",
+        "TICK_FORMAT": ".2%"
+    },
+    "CONVERSION_RATE": {
+        "HOVER_DATA": "point",
+        "TICK_FORMAT": ".2%"
+    }
+}
 
 if __name__ == '__main__':
     print("This file is not meant to be run on its own.")
